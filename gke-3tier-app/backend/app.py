51fc32ef66018c0afcbfcbad81874298a11a395e
NiranPrem <163099269+NiranPrem@users.noreply.github.com>
2025-09-16 09:18:48 +0000

    Update app.py

---
+++ gke-3tier-app/backend/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+import os, mysql.connector
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME", "mysql")
+
+@app.route("/")
+def home():
+    return "Hello from GKE Backend"
+
+@app.route("/api")
+def api():
+    try:
+        conn = mysql.connector.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME,
+            connection_timeout=5
+        )
+        cur = conn.cursor()
+        cur.execute("SELECT NOW();")
+        now = cur.fetchone()
+        cur.close()
+        conn.close()
+        return jsonify({"db": "ok", "time": str(now)})
+    except Exception as e:
+        return jsonify({"db": "error", "error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
