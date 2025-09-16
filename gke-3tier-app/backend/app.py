b404587519d86b23f433270071083af6d4ef56ca
NiranPrem <163099269+NiranPrem@users.noreply.github.com>
2025-09-16 09:21:36 +0000

    Update app.py

--- gke-3tier-app/backend/app.py
+++ gke-3tier-app/backend/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
 DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "StrongPass123")
 DB_NAME = os.getenv("DB_NAME", "mysql")
 
 @app.route("/")
